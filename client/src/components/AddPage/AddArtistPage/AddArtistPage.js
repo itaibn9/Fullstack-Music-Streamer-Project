69e7e3778ff155aaea5703221243258f4adf6064
itaibn9 <itaibn9@gmail.com>
2020-09-26 22:03:43 +0300

    artist page started edit

--- client/src/components/AddPage/AddArtistPage/AddArtistPage.js
+++ client/src/components/AddPage/AddArtistPage/AddArtistPage.js
@@ -1,10 +1,26 @@
-import React from 'react'
-
+import React from 'react';
+import './AddArtistPage.css';
 function AddArtistPage() {
+    const postArtist = ()=>{
+        return true
+    }
     return (
-        <div>
-            <h1>Hello Artist</h1>
-        </div>
+        <form className="artistAddPage">
+            <div className="form__group field">
+            <input className="form__field" placeholder="Artist name" required/>
+            <label className="form__label">Artist name</label>
+            </div>
+            <div className="form__group field">
+            <input className="form__field" placeholder="Cover image" required/>
+            <label className="form__label">Cover image</label>
+            </div>
+            <div className="form__group field">
+            <input className="form__field" placeholder="Description" required/>
+            <label className="form__label">Description</label>
+            </div>
+            <button type="submit" onSubmit={postArtist}>Add Artist</button>
+            </form>
+       
     )
 }
 
