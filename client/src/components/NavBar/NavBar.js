1224658350c337f6239e957451b249726ce63cb7
itaibn9 <itaibn9@gmail.com>
2020-09-26 19:22:06 +0300

    added password to open the add page

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React,{ useState } from 'react';
 import { Link } from "react-router-dom";
 import logo from './gitaure.png';
 import './navbar.css';
 import person from './person.png';
+require('dotenv').config();
+const notSafePassword = process.env.REACT_APP_ADMINPASSWORD;
 function NavBar() {
+  const [admin, setAdmin] = useState(false);
+
+  const adminValidation = () => {
+   const password =  prompt('please enter the password: ');
+    if(password === notSafePassword){
+      setAdmin(true);
+    }else{
+      alert("Wrong Password")
+    }
+  }
   return (
     <nav className="navbar"> 
     <div className="navbar__container">
@@ -13,9 +25,12 @@ function NavBar() {
               </Link>
                 <div className="navbar__line" />
                 </div>
-                <div className="navbar__container">
-              <Link className="navbar__links" to="/api/search">Search</Link>      
-                <Link  className="navbar__links" to="/api/add">Add</Link>
+                <div className="navbar__container">{admin ? 
+                <Link className="navbar__links" to="/api/add">Add</Link> :
+                <button className="navbar__links" onClick={()=> adminValidation()}>Add</button> 
+                }
+              <Link className="navbar__links" to="/api/search">Search</Link>
+                  
                 </div>
                 <div className="navbar__container">
                 <div className="navbar__line" />
