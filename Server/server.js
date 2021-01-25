a7555098ca5fc676bc4c916cceca3bd39bd822d7
itaibn9 <itaibn9@gmail.com>
2021-01-25 11:53:18 +0200

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -2,6 +2,6 @@ const app = require('./app');
 require('dotenv').config();
 const port = process.env.PORT || 8080;
 
-app.listen(port, () => {
+app.listen(process.env.PORT || 8080, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
